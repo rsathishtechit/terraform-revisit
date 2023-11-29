1115151644bb85ea5bffaddb3740ab26731b561e
Sathishkumar R <rsathishtechit@gmail.com>
2023-11-29 18:50:10 +0530

    Added using key from local for ssh into ec2

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable avail_zone {}
 variable env_prefix{}
 variable my_ip {}
 variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_blocks
@@ -91,12 +92,17 @@ data "aws_ami" "latest-amazon-linux-image" {
   } 
 }
 
-# output "aws_ami_id" {
-#   value = data.aws_ami.latest-amazon-linux-image.id
-# }
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
 
 output "ec2_public_ip" {
     value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "myapp-ec2-server-key"
+  public_key = file(var.public_key_location)
 }
 
 
@@ -110,7 +116,7 @@ resource "aws_instance" "myapp-server" {
 
   associate_public_ip_address = true
 
-  key_name = "ec2"
+  key_name = aws_key_pair.ssh-key.key_name
 
   tags = {
     Name = "${var.env_prefix}-server"

433bbc61635a0a526a84d9554960d90c2506dabe
Sathishkumar R <rsathishtechit@gmail.com>
2023-11-29 15:06:29 +0530

    Added ec2 instance creation

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix{}
 variable my_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_blocks
@@ -47,8 +48,7 @@ resource "aws_route_table_association" "aws-route-table-association" {
   route_table_id = aws_route_table.myapp-route-table.id
 }
 
-resource "aws_security_group" "myapp-security-group" {
-  name = "myapp-security-group"
+resource "aws_default_security_group" "default-security-group" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -89,4 +89,30 @@ data "aws_ami" "latest-amazon-linux-image" {
     name = "virtualization-type"
     values = ["hvm"]
   } 
+}
+
+# output "aws_ami_id" {
+#   value = data.aws_ami.latest-amazon-linux-image.id
+# }
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+
+resource "aws_instance" "myapp-server" {
+  ami =  data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-security-group.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+
+  key_name = "ec2"
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 }
